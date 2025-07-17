0c0b5eafc3132f5b2ac97f898445fc8dead8c33a
ImJopeh <jopedev@gmail.com>
2025-07-17 20:28:02 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,4 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyDyF2VyiIK1DbZE29AdzkLYCSU4HB1z9qw",
   authDomain: "form-project-da2d8.firebaseapp.com",
