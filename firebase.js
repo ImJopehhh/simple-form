b82ff2d3583d2885cd90ddd627af565b9a890b03
ImJopeh <jopedev@gmail.com>
2025-07-17 20:34:31 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,8 @@
-// firebase.js
 const firebaseConfig = {
   apiKey: "AIzaSyDyF2VyiIK1DbZE29AdzkLYCSU4HB1z9qw",
   authDomain: "form-project-da2d8.firebaseapp.com",
   projectId: "form-project-da2d8",
-  storageBucket: "form-project-da2d8.firebasestorage.app",
+  storageBucket: "form-project-da2d8.appspot.com",
   messagingSenderId: "864862750726",
   appId: "1:864862750726:web:5e9ed7379e633df5e0db87"
 };
